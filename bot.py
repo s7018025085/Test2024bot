52931d8436137003a4a196e2b0b9b7cd0b209b78
Madi123 <s7018025085@gmail.com>
2024-06-26 17:24:03 +0500

    Initial commit7

--- bot.py
+++ bot.py
@@ -1,63 +1,38 @@
 import telebot
-import requests
+from flask import Flask, request
 import os
 
 API_TOKEN="7303306074:AAEMtjoNCucLezlv_f__79iMSlRqn7lP3JQ"
 WEBHOOK_URL = "https://Test2024bot.up.railway.app/webhook" # URL вебхука с Railway
 
-bot = telebot.TeleBot(API_TOKEN)  
+
+bot = telebot.TeleBot(API_TOKEN)
+app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Добро пожаловать в бот тестирование! Используйте /quiz, чтобы задать вопрос.")
+    bot.reply_to(message, "Добро пожаловать в бот!")
 
 @bot.message_handler(commands=['quiz'])
-def send_question(message): 
-    response = requests.get(QUESTIONS_URL)
-    questions = response.json() 
-    question = random.choice(questions)
-    options = '\n'.join([f"{i + 1}. {opt}" for i, opt in enumerate(question["options"])])
-    msg = f"{question['question']}\n\n{options}"
-    bot.send_message(message.chat.id, msg)
+def send_question(message):
+    # Ваша логика викторины здесь
+    pass
 
-    bot.register_next_step_handler(message, lambda msg: check_answer(msg, question))
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
-def check_answer(message, question):
-    try:
-        answer_index = int(message.text.strip()) - 1
-        if question["options"][answer_index] == question["answer"]:
-            bot.send_message(message.chat.id, "Correct!")
-        else:
-            bot.send_message(message.chat.id, "Wrong answer.")
-    except (ValueError, IndexError):
-        bot.send_message(message.chat.id, "Please enter a valid option number.")
-
-@bot.route('/webhook', methods=['POST'])
-def webhook_handler():
-    if flask.request.headers.get('content-type') == 'application/json':
-        json_str = flask.request.get_data().decode('UTF-8')
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_str = request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_str)
         bot.process_new_updates([update])
         return ''
     else:
-        flask.abort(403)
-
-# Установка вебхука
-bot.remove_webhook()
-bot.set_webhook(url=WEBHOOK_URL)
+        return 'Неверный Content-Type'
 
 if __name__ == '__main__':
-    from flask import Flask, request
-    app = Flask(__name__)
-
-    @app.route('/webhook', methods=['POST'])
-    def webhook():
-        if request.headers.get('content-type') == 'application/json':
-            json_str = request.get_data().decode('UTF-8')
-            update = telebot.types.Update.de_json(json_str)
-            bot.process_new_updates([update])
-            return ''
-        else:
-            flask.abort(403)
-
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
